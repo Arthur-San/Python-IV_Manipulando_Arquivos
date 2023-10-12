8019be62e66783e1787641955891082c6cd3f72c
Arthur-San <arthursantos242568@gmail.com>
2023-10-11 22:46:44 -0300

    amanha eu termino

--- Agenda.py
+++ Agenda.py
@@ -7,3 +7,18 @@ def add_contato(nome, numero):
     with open("dados/exercicioFinal.txt", "a", encoding="utf-8") as file:
         file.write(f"\n{nome}/{numero}")
 
+
+def list_contatos():
+    with open("dados/exercicioFinal.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            print(line.rstrip())
+
+def remove_contato(nome, numero):
+    with open("dados/exercicioFinal.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            if line == nome and line == numero:
+                print('achei')
+
+def limpar_console():
+    import os
+    os.system('cls')
--- exercicioFinal.py
+++ exercicioFinal.py
@@ -7,10 +7,37 @@
 #     2. Um método para listar contatos.
 #     3. Um método para remover contatos.
 # 2. Criar um arquivo principal para a aplicação que importar o módulo de agenda e chamar cada um dos métodos desenvolvidos de acordo com a opção escolhida.
-from Agenda import add_contato
+from Agenda import add_contato, list_contatos,limpar_console
+
+choice = 0
 
 class Main:
-    nome = input('Digite o nome: ')
-    numero = int(input('Digite o numero: '))
+    while choice != 5:
+        print('------AGENDA------')
+        print('1 - Adicionar contato')
+        print('2 - Listar contatos')
+        print('3 - Remover contato')
+        print('4 - Limpar console')
+        print('5 - SAIR')
 
-    contato = add_contato(nome, numero)
+        choice = int(input('O que deseja fazer?\n->'))
+
+        if choice == 1:
+            nome = input('Digite o nome: ')
+            numero = int(input('Digite o numero: '))
+            contato = add_contato(nome, numero)
+        elif choice == 2:
+                list_contatos()
+        #elif choice == 3:
+        elif choice == 4:
+            limpar_console()
+            
+        elif choice == 5:
+             print('Programa encerrado!')
+             break
+            
+        
+
+        
+
+        
